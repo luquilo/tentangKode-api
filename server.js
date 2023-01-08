37c164aa4d9cd785f850292b7b5f0222b5fc9d7e
luquilo <luquilo.etc@gmail.com>
2023-01-08 08:48:11 +0700

    coba

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+const corsOptions = {
+    origin: '*'
+}
+
+// register cors middleware
+app.use(cors(corsOptions))
+
+// register middleware untuk parsing json yang diterima dari body request
+app.use(express.json())
+
+//membuat routes
+app.get('/', (req,res) => {
+    res.json({message: 'hello'})
+})
+
+const PORT = process.env.PORT || 8000
+app.listen(PORT, () => console.log(`server run on port ${PORT}`))
+
